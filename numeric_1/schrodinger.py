4b60e0bc481d9dfd82d1e95ba995349ff066690e
William Dugan <95777710+willidu@users.noreply.github.com>
2022-10-13 15:21:37 +0200

    Update schrodinger.py

--- numeric_1/schrodinger.py
+++ numeric_1/schrodinger.py
@@ -4,11 +4,9 @@ This module is an implementation for solving the time-independent Schrödinger e
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.constants
 from scipy.linalg import eigh_tridiagonal
 
-BOHR_RADII = scipy.constants.physical_constants['Bohr radius'][0]
-COLORS = ['g', 'b', 'r', 'c']
+COLORS = ['g', 'b', 'r', 'm']
 
 def schrodinger(
         potential: np.ndarray,
@@ -28,17 +26,16 @@ def schrodinger(
     Returns
     -------
     E : np.ndarray
-        Energy levels in eV (eigenvalues)
+        Energy levels in Hartree (eigenvalues)
     Psi : np.ndarray
         Normalized wave functions (eigenfunctions)
     """
     diag = 1. / (dx ** 2) + potential
     semidiag = - 1. / (2. * dx ** 2) * np.ones(len(potential) - 1)
     eigvals, eigvecs = eigh_tridiagonal(diag, semidiag)
-    eigvecs = eigvecs.T
-    eigvecs /= np.sqrt(dx)
+    eigvecs = eigvecs.T / np.sqrt(dx)
     assert np.allclose(np.einsum('ij,ij->i', eigvecs, eigvecs)*dx, np.ones(len(eigvecs))), 'Psi not normalized'
-    return eigvals / scipy.constants.physical_constants['electron volt-hartree relationship'][0], eigvecs
+    return eigvals, eigvecs
 
 
 def plot(
@@ -58,24 +55,24 @@ def plot(
     fig.subplots_adjust(hspace=.4, wspace=0.3)
 
     # Upper left plot
-    for i in range(3):
+    for i in range(len(COLORS)):
         ax[0,0].plot(x, psi[i], label=f'n = {i+1}', color=COLORS[i])
 
     if psi_analytical is not None:
-        for n, psi_n in enumerate(psi_analytical):
-            ax[0,0].plot(x, psi_n, label=f'$\psi_{n+1}$', color=COLORS[n], ls='--')
+        for n in range(min(len(psi_analytical), len(COLORS))):
+            ax[0,0].plot(x, psi[n], label=f'$\psi_{n+1}$', color=COLORS[n], ls='--')
 
     ax[0,0].set(
         ylabel=r'$\Psi$',
         xlabel='Distance x/L',
         xlim=(np.min(x), np.max(x)),
-        title='Wave function of bound state'
+        title='Wave function'
     )
-    ax[0,0].legend(ncol=3, loc='lower left')
+    ax[0,0].legend(ncol=4, loc='lower left')
     ax[0,0].grid(True)
 
     # Upper right plot
-    for i in range(3):
+    for i in range(min(len(COLORS), 3)):
         ax[0,1].plot(x, psi[i]**2, label=f'n = {i+1}', color=COLORS[i])
 
     ax[0,1].set(
@@ -88,18 +85,17 @@ def plot(
     ax[0,1].grid(True)
 
     # Lower left plot
-    for i in range(4):
+    for i in range(len(COLORS)):
         ax[1,0].axhline(energy[i], label=f'n = {i+1}', color=COLORS[i])
 
     # Plots analytical levels if given
     if energy_lvls_analytical is not None:
-        for i, energy_level in enumerate(energy_lvls_analytical):
-            ax[1,0].axhline(energy_level, label=f'$E_{i+1}$', color=COLORS[i], ls='--')
+        for n in range(min(len(energy_lvls_analytical), len(COLORS))):
+            ax[1,0].axhline(energy_lvls_analytical[n], label=f'$E_{n+1}$', color=COLORS[n], ls='--')
 
     ax[1,0].set(
         xlabel='Distance x/L',
-        ylabel='Energy [eV]',
-        xlim=(np.min(x), np.max(x)),
+        ylabel='Energy [Hartree]',
         title='Energy levels'
     )
     ax[1,0].legend(loc='upper left', ncol=4)
